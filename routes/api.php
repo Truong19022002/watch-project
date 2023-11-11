1e47a841f578d2a0009eab49754b048f750d74bc
Trickey <vngthnh999@gmail.com>
2023-11-11 18:09:22 +0700

    feat: BillSale vs DetailBillSale

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ Route::get('/type', [TypeController::class, 'get']);
 Route::apiResource('cart', CartController::class);
 
 Route::get('/vnpay_payment', [CheckoutController::class, 'vnpay_payment']);
+Route::get('/bill', [CheckoutController::class, 'updateBill']);
 
 Route::middleware(['user'])->prefix('/auth/admin')->group(function() {
     Route::post('/login', [AuthController::class, 'login']);
